557f55b360bf661d28ca77c887073754a88e19ac
Ben Perry <bhperry94@gmail.com>
2024-08-23 11:49:26 -0500

    Benchmark command

--- tools/cmd/root.go
+++ tools/cmd/root.go
@@ -7,16 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "pixeltools",
-	Short: "Pixel tools provide benchmarking and validation tools for developing the pixel library",
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
-	},
+func NewRootCmd() *cobra.Command {
+	root := &cobra.Command{
+		Use:   "pixeltools",
+		Short: "Pixel tools provide benchmarking and validation tools for developing the pixel library",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	root.AddCommand(NewBenchCmd())
+	return root
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := NewRootCmd().Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
